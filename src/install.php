c41f167ffbdaefef891f341d8bac4514a359fd48
Homayun Gohari <30705391+Homayuun@users.noreply.github.com>
2025-09-09 09:33:20 +0330

    add dummy user and dummy notes

--- src/install.php
+++ src/install.php
@@ -25,4 +25,18 @@ foreach ($queries as $name => $sql) {
         echo "Error creating $name: " . $connection->error . "<br>";
     }
 }
+
+$insertUser = "REPLACE INTO UsersTable (username, password) VALUES ('admin', 'admin')";
+$insertNotes = "REPLACE INTO NotesTable (title, content) VALUES 
+('title 1', 'content 1'),('title 2', 'content 2'),('title 3', 'content 3'),('title 4', 'content 4'),('title 5', 'content 5'),
+('title 6', 'content 6'),('title 7', 'content 7'),('title 8', 'content 8'),('title 9', 'content 9'),('title 10', 'content 10')";
+
+
+if (!$connection -> query($insertUser)) {
+    echo "Error inserting default user: " . $connection->error . "<br>";
+}
+
+if (!$connection -> query($insertNotes)) {
+    echo "Error inserting default notes: " . $connection->error . "<br>";
+}
 ?>
