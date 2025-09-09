66a23362a4a35d38a16a87fa790e9584a187755a
Homayun Gohari <30705391+Homayuun@users.noreply.github.com>
2025-09-09 10:56:12 +0330

    update auth by userid and username global variables

--- src/auth.php
+++ src/auth.php
@@ -5,3 +5,6 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['username'])) {
     header("Location: login.php");
     exit;
 }
+
+$currentUserId = $_SESSION['user_id'];
+$currentUsername = $_SESSION['username'];
