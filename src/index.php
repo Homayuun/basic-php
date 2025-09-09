b359b2a397711bf45a3464512fb50448baeda9d3
Homayun Gohari <30705391+Homayuun@users.noreply.github.com>
2025-09-09 15:42:55 +0330

    last commit before refactoring with ajax

--- src/index.php
+++ src/index.php
@@ -33,9 +33,9 @@ $offset = ($page - 1) * $notesPerPage;
 
 $totalNotes = getTotalNotes($connection, $currentUserId);
 $totalPages = ceil($totalNotes / $notesPerPage);
-
 $notes = getNotes($connection, $currentUserId, $notesPerPage, $offset);
 ?>
+
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
@@ -43,7 +43,6 @@ $notes = getNotes($connection, $currentUserId, $notesPerPage, $offset);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-4">
-
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h1>Notes</h1>
         <div>
